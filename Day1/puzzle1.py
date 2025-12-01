b223a3cc68ae2f4a94f85e21b0317974101ead30
Elijah Brown <ebrown@specterops.io>
2025-12-01 15:13:11 -0700

    Save progress on puzzle 1

---
+++ Day1/puzzle1.py
@@ -0,0 +1,47 @@
+
+
+import sys
+
+
+safeDial = 50
+
+# wrap safeDial between 0 - 100
+def AddSafeDial(value: int):
+    global safeDial
+    
+    safeDial += value
+    if safeDial >= 100:
+        safeDial = safeDial % 100
+    if safeDial < 0:
+        safeDial += 100
+
+
+def main():
+    global safeDial
+    zeroCounter = 0
+
+    if len(sys.argv) < 2:
+        print("Usage: python puzzle1.py <filename>")
+        return
+    
+
+    with open(sys.argv[1], "r") as f:
+        for line in f:
+            leftOrRight = line[0]
+            value = int(line[1:])
+            if (leftOrRight == "L"):
+                AddSafeDial(-value)
+            else:
+                AddSafeDial(value)
+
+            print("The dial is rotated {} to point at {}".format(line.strip(), safeDial))
+            if safeDial == 0:
+                zeroCounter += 1
+                #print("we got a zero!")
+        
+        print("The password is ", zeroCounter)
+
+
+
+if __name__ == "__main__":
+    main()
