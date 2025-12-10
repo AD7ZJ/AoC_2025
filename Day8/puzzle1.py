f59bddf2562ba17a3907eda959d873c51e3a1c30
Elijah Brown <ebrown@specterops.io>
2025-12-10 00:13:00 -0700

    Getting closer finally...

---
+++ Day8/puzzle1.py
@@ -0,0 +1,116 @@
+import math
+import sys
+
+def main():
+    global distanceCache
+    if len(sys.argv) < 2:
+        print("Usage: python puzzle1.py <filename>")
+        return
+    
+    pointList = []
+
+    with open(sys.argv[1], "r") as f:
+        for line in f:
+            line = line.strip()
+            x,y,z = line.split(',')
+            pointList.append((int(x),int(y),int(z)))
+
+    p1, p2 = FindClosestPairBf(pointList)
+    print(f"Distance between {p1} and {p2} is {DistBetween2Points(p1, p2)}")
+
+    # sort based on the distance (element 0 of the tuple)
+    distanceCache.sort(key=lambda tup: tup[0])
+
+    for thing in distanceCache[:10]:
+        print(thing)
+
+    circuitList = [{distanceCache[0][1], distanceCache[0][2]}]
+    for entry in distanceCache[:10]:
+        p1 = entry[1]
+        p2 = entry[2]
+
+        g1 = FindCircuitGroup(circuitList, p1)
+        g2 = FindCircuitGroup(circuitList, p2)
+
+        if g1 == None and g2 == None:
+            # start a new set
+            circuitList.append({p1, p2})
+            print(f"started a new group {p1} and {p2}")
+        elif g1 != None and g2 == None:
+            g1.add(p2)
+            print(f"added {p2}")
+        elif g1 == None and g2 != None:
+            g2.add(p1)
+            print(f"added {p1}")
+        elif g1 != g2:
+            # merge these sets, this point pair is connecting them together
+            g1.update(g2)
+            circuitList.remove(g2)
+            print(f"merged two sets")
+    
+
+    for thing in circuitList:
+        print(thing)
+
+
+
+def FindCircuitGroup(list, point):
+    for circuit in list:
+        if point in circuit:
+            return circuit
+    return None
+
+
+distanceCache = []
+pointsSeen = set()
+def FindClosestPairBf(list):
+    minDistance = 1e10
+    minDistP1 = None
+    minDistP2 = None
+    for i in range(len(list)):
+        for j in range(len(list)):
+            if i != j:
+                p1 = list[i]
+                p2 = list[j]
+                dist = DistBetween2Points(p1, p2)
+                # avoid caching reversed pairs as dupes
+                if (p1, p2) not in pointsSeen and (p2, p1) not in pointsSeen:
+                    pointsSeen.add((p1, p2))
+                    distanceCache.append((dist, p1, p2))
+
+                if dist < minDistance:
+                    minDistance = dist
+                    minDistP1 = p1
+                    minDistP2 = p2
+
+    return minDistP1, minDistP2
+
+def FindClosestToListOfPoints(listToSearch, myList):
+    minDistance = 1e10
+    minDistP1 = None
+    minDistP2 = None
+    for i in range(len(listToSearch)):
+        for j in range(len(myList)):
+            p1 = listToSearch[i]
+            p2 = myList[j]
+            if p1 != p2:
+                dist = DistBetween2Points(p1, p2)
+                if dist < minDistance:
+                    minDistance = dist
+                    minDistP1 = list[i]
+                    minDistP2 = list[j]
+
+
+def DistBetween2Points(p1, p2):
+    return math.sqrt(
+        (p1[0] - p2[0])**2 +
+        (p1[1] - p2[1])**2 +
+        (p1[2] - p2[2])**2
+    )
+
+def PrintMatrix(matrix):
+    for row in matrix:
+        print("".join(row))
+
+if __name__ == "__main__":
+    main()
